f98e545e5bd1353dcd3cd4cd5bd3d801fc0e0f6c
fpsampayo <fpsampayo@gmail.com>
2017-06-30 11:47:01 +0200

    Se añade evento para gestionar la busqueda

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
+import 'jquery'
+import 'bootstrap'
 import Map from './map'
+import 'bootstrap/dist/css/bootstrap.css'
 import './styles/styles.css'
+import CatastroParser from './catastroParser'
 
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const map = new Map()
+const catastroParser = new CatastroParser()
+const btnSearch = document.getElementById('btn-search')
+
+
+btnSearch.addEventListener('click', (evt) => {
+  evt.stopPropagation()
+  const refCat = document.getElementById('txt-refcat').value
+  /*
+  var json = catastroParser.getParcel(refCat)
+
+  map.loadGeoJson(json)
+  */
+  catastroParser.getParcel(refCat).then((geoJson) => {
+    map.loadGeoJson(geoJson)
+  })
+
+})
 
