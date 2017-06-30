3b7c4deeac6924f103c798505d672a2a45db2cf3
fpsampayo <fpsampayo@gmail.com>
2017-06-30 11:49:57 +0200

    Se limpia el codigo

--- src/catastroParser.js
+++ src/catastroParser.js
@@ -1,37 +1,4 @@
-/*
-import Jsonix from '@boundlessgeo/jsonix'
-import XLink_1_0 from 'w3c-schemas'
-import XSD_1_0 from 'w3c-schemas'
-import OWS_1_1_0 from 'w3c-schemas'
-import WFS_2_0 from 'ogc-schemas'
-import GML_3_2_1 from 'ogc-schemas'
-import ISO19139_GMD_20070417 from 'ogc-schemas'
-import Filter_2_0 from 'ogc-schemas'
-import net_opengis_gml__3 from '../jsonix/tmp/mapping/net_opengis_gml__3'
-import org_isotc211__2005_gmd from '../jsonix/tmp/mapping/org_isotc211__2005_gmd'
-import org_w3__1999_xlink from '../jsonix/tmp/mapping/org_w3__1999_xlink'
-*/
 
-/*
-console.log(Jsonix.Jsonix)
-
-const context = new Jsonix.Jsonix.Context(
-  [
-    GML_3_2_1,
-    net_opengis_gml__3,
-    org_isotc211__2005_gmd,
-    org_w3__1999_xlink
-  ]
-)
-
-const unmarshaller = context.createUnmarshaller()
-
-unmarshaller.unmarshalURL("https://crossorigin.me/http://ovc.catastro.meh.es/INSPIRE/wfsCP.aspx?service=wfs&version=2&request=getfeature&STOREDQUERIE_ID=GetParcel&REFCAT=36055A01000100&srsname=EPSG:25829",
-  function (data)	{
-	var objectFromURL = data
-    console.log(objectFromURL)
-  })
-*/
 
 export default class CatastroParser {
   constructor(){
