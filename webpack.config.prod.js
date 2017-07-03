554ccd2dacd578ad1583b8cc688444a0a3ab6bc4
fpsampayo <fpsampayo@gmail.com>
2017-07-03 15:42:17 +0200

    Configuracion webpack

---
+++ webpack.config.prod.js
@@ -0,0 +1,66 @@
+var path = require('path');
+const entryPath = path.join(__dirname, 'src'),
+  outputPath = path.join(__dirname, 'dist');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: "source-map",
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/js'),
+    filename: '[name].js',
+    publicPath: "./js/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/],
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    // Leaflet image Alias resolutions
+    alias: {
+      './images/layers.png$': path.resolve(__dirname, './node_modules/leaflet/dist/images/layers.png'),
+      './images/layers-2x.png$': path.resolve(__dirname, './node_modules/leaflet/dist/images/layers-2x.png'),
+      './images/marker-icon.png$': path.resolve(__dirname, './node_modules/leaflet/dist/images/marker-icon.png'),
+      './images/marker-icon-2x.png$': path.resolve(__dirname, './node_modules/leaflet/dist/images/marker-icon-2x.png'),
+      './images/marker-shadow.png$': path.resolve(__dirname, './node_modules/leaflet/dist/images/marker-shadow.png')
+    }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(), // Enable HMR
+    new webpack.NamedModulesPlugin(),
+    new webpack.ProvidePlugin({
+      jQuery: 'jquery',
+      $: 'jquery',
+      jquery: 'jquery'
+    })
+  ],
+  node: {
+    fs: 'empty',
+  }
+};
