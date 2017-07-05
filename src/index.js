b59aad508769546a9f1326d0dd46d0e766b5c94e
fpsampayo <fpsampayo@gmail.com>
2017-07-05 11:18:34 +0200

    Se añade traducción al datepicker

--- src/index.js
+++ src/index.js
@@ -46,14 +46,38 @@ $(".button-collapse").sideNav({
 })
 
 $('.datepicker').pickadate({
+
+  // The title label to use for the month nav buttons
+  labelMonthNext: 'Mes siguiente',
+  labelMonthPrev: 'Mes anterior',
+
+// The title label to use for the dropdown selectors
+  labelMonthSelect: 'Selecciona un mes',
+  labelYearSelect: 'Selecciona un año',
+
+// Months and weekdays
+  monthsFull: [ 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre' ],
+  monthsShort: [ 'Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic' ],
+  weekdaysFull: [ 'Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado' ],
+  weekdaysShort: [ 'Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab' ],
+
+// Materialize modified
+  weekdaysLetter: [ 'D', 'L', 'M', 'X', 'J', 'V', 'S' ],
+
+// Today and clear
+  today: 'Hoy',
+  clear: 'Limpiar',
+  close: 'Cerrar',
+
   format: 'dd/mm/yyyy',
-  closeOnSelect: true,
-  closeOnClear: true,
+  //closeOnSelect: true,
+  //closeOnClear: true,
   selectMonths: true, // Creates a dropdown to control month
   selectYears: 15, // Creates a dropdown of 15 years to control year
   container: document.body, 
   onSet: function(e) {
     setDate()
+    this.close()
   }
 })
 
